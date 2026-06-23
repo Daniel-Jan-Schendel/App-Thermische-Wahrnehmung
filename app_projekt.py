c14daa8cba6a809072e78d2ee97fe72a70df1f8b
Mirtha-Pillaca <130111517+Mirtha-Pillaca@users.noreply.github.com>
2026-06-23 22:59:09 +0200

    Add files via upload

---
+++ app_projekt.py
@@ -0,0 +1,21 @@
+import warnings 
+import streamlit as st
+from styles import apply_styles
+
+apply_styles()
+
+st.sidebar.image("logo.png", width=500)
+
+st.set_page_config(page_title="ASHRAE", layout="wide", initial_sidebar_state="expanded")
+pg = st.navigation(
+    [
+        st.Page("welcome.py", title="Welcome", icon=":material/groups:"),
+        st.Page("introduction.py", title="Main Menu", icon=":material/home:"),
+        st.Page("dashboard.py", title="Dashboard", icon=":material/menu_book:"),
+        st.Page("database.py", title="Database", icon=":material/dashboard:"),
+        st.Page("datenbereinigung.py", title="Datenbereinigung", icon=":material/menu_book:"),
+        st.Page("analytics.py", title="Analytics", icon=":material/analytics:"),
+        st.Page("machine_learning.py", title="Machine Learning", icon=":material/smart_toy:")
+    ]
+)
+pg.run()
