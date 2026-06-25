59601e85ace0e14e68f433b46e1914a3abb9fd1e
klotz79797979-eng <klotz79797979@gmail.com>
2026-06-25 15:22:06 +0200

    test_pull

--- machine_learning.py
+++ machine_learning.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import sklearn
+import matplotlib.pyplot as pd
 
 st.set_page_config(page_title="Machine Learning - ASHRAE", layout="wide",initial_sidebar_state="expanded")
 
